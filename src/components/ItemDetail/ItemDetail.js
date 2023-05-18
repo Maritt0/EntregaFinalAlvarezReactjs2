f77f58fe45c5cefde7d691efc92cc4a0da651d85
Maritt0 <121215555+Maritt0@users.noreply.github.com>
2023-05-18 20:46:25 -0300

    Add files via upload

---
+++ src/components/ItemDetail/ItemDetail.js
@@ -0,0 +1,34 @@
+import './ItemDetail.css';
+import ItemCount from '../ItemCount/ItemCount';
+
+
+const ItemDetail = ({id, name, img, category, description, price, stock }) => {
+    return(
+        <article className='CardItem'>
+            <header className='Header'>
+                <h2 className='ItemHeader'>
+                {name}
+                </h2>
+                </header>
+                <picture>
+                    <img src={img} alt={name} className='ItemImg'/>
+                </picture>
+                <section>
+                    <p className='Info'>
+                        Categoría: {category}
+                    </p>
+                    <p className='Info'>
+                        Categoría: {description}
+                    </p>
+                    <p className='Info'>
+                        Categoría: {price}
+                    </p>
+                </section>
+                <footer className='ItemFooter'>
+                    <ItemCount initial={1} stock={stock} onAdd={(quantity) => console.log('Cantidad agregada', quantity)}></ItemCount>
+                </footer>
+                </article>
+)
+}
+
+export default ItemDetail
