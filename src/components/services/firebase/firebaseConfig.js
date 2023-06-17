7e6220390110c070092aeeea6d5ede59b18146b9
Maritt0 <121215555+Maritt0@users.noreply.github.com>
2023-06-16 23:22:19 -0300

    Update firebaseConfig.js

--- src/components/services/firebase/firebaseConfig.js
+++ src/components/services/firebase/firebaseConfig.js
@@ -1,1 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/analytics";
 
+const firebaseConfig = {
+  apiKey: "AIzaSyBTmcQZ1MegkujACntd12bE2uC4_1j0AXA",
+  authDomain: "proyecto-final-alvarez-reactjs.firebaseapp.com",
+  projectId: "proyecto-final-alvarez-reactjs",
+  storageBucket: "proyecto-final-alvarez-reactjs.appspot.com",
+  messagingSenderId: "1085834887294",
+  appId: "1:1085834887294:web:05b9a3a2cf8d21ecea182f",
+  measurementId: "G-2DMZSZ0WQX"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app)
